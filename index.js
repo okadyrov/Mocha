969ee39700cdc015e510f6472a6bcada467ccace
okadyrov <58760195+okadyrov@users.noreply.github.com>
2020-01-08 19:09:34 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -4,5 +4,25 @@ function sum(a, b) {
 function mult(a, b){
     return a * b;
 }
+function isEvenNumber(number){
 
-module.exports = {sum, mult};
+    return number % 2 === 0;
+}
+function isAdult(age){
+    if(age > 20){
+        return true
+    } else if(age >= 0 && age < 20){
+        return false
+    } else{
+        return undefined;
+    }
+}
+function isPalindrome(str) {
+    if(typeof str !== "string")
+        return 'not string';
+    strReverse = str.split('').reverse().join('');
+    if (strReverse == str) {
+        return true;
+    }
+}
+module.exports = {sum, mult, isEvenNumber, isAdult, isPalindrome};
