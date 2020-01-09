9ed8583c2699e7057d94c0be89fa1cc3f874aed9
okadyrov <58760195+okadyrov@users.noreply.github.com>
2020-01-08 19:10:15 -0600

    Add files via upload

--- index.spec.js
+++ index.spec.js
@@ -1,16 +1,42 @@
 const assert = require('assert');
-const {sum, mult} = require('../index.js');
+const {sum, mult, isEvenNumber, isAdult, isPalindrome } = require('../index.js');
 
 describe('function sum', () => {
     it('should function sum equals 10', () => {
         assert.equal(sum(2, 4), 6);
     })
+
     it('should variable function sum not equals 0', () => {
+
         assert.notEqual(sum(2, 4), 0)
     })
 })
 
-
 it('should mult equals 8', () => {
     assert.equal(mult(2, 4), 8);
 });
+describe('function isEvenNumber',()=> {
+
+    it('should return true for even number', function () {
+
+        assert.equal(isEvenNumber(40), true)
+
+    });
+});
+describe('function isAdult',()=>{
+    it('should return true if age is over 20',function(){
+        assert.equal(isAdult(57), true)
+    });
+});
+describe('function Palendrome',() =>{
+    it('should return true if str Palindrome', () => {
+        assert.equal(isPalindrome('abcba'),true)
+    })
+    it('if the input is not string',()=>{
+        assert.equal(isPalindrome(999),"not string")
+    })
+});
+
+
+
+
